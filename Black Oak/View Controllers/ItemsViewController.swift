cd6580633f29c9cf5e5be2646ecef0e6ba6c9037
Jay Patel <jayp@blkbox.ai>
2022-03-05 11:19:17 -0500

    Dummy CollectionView

---
+++ Black Oak/View Controllers/ItemsViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ItemsViewController.swift
+//  Black Oak
+//
+//  Created by Jay Patel on 05/03/22.
+//
+
+import UIKit
+
+class ItemsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    @IBOutlet weak var itemCollectionView: UICollectionView!
+    
+    var items = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "itemCell", for: indexPath) as! ItemCell
+        //items[indexpath.row]
+        return cell
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width / 2 - 5, height: collectionView.bounds.width / 3 - 5)
+    }
+        
+    //space between rows top and bottom rows
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+    
+    //Space between cells in same row
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+    
+}
+
+
+class ItemCell : UICollectionViewListCell {
+    
+    @IBOutlet weak var imgView: UIImageView!
+}
