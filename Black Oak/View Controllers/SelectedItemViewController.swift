9c5ef12ef514040b7f6da28473e00e9c37da6a97
yash3915 <yashpatel.m19@gpahmedabad.ac.in>
2022-03-13 19:17:42 +0530

    item page added

---
+++ Black Oak/View Controllers/SelectedItemViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SelectedItemViewController.swift
+//  Black Oak
+//
+//  Created by Yash Patel on 13/03/22.
+//
+
+import UIKit
+
+class SelectedItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
